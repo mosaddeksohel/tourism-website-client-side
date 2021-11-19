8aa20e302f99b68b5be469796dd99936dabbfa8b
Mosaddek Sohel <tanjimsohel>
2021-11-19 21:55:10 +0600

    first commit

--- src/Page/Firebase/Firebase.confing.js
+++ src/Page/Firebase/Firebase.confing.js
@@ -1,18 +1,13 @@
 const firebaseConfig = {
-    // apiKey: process.env.REACT_APP_API_KEY,
-    // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    // projectId: process.env.REACT_APP_PROJECT_ID,
-    // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    // appId: process.env.REACT_APP_APP_ID
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 
 
-    apiKey: "AIzaSyB1Q2yozYCWEr1udb3Varkpx9a0IlTipY4",
-    authDomain: "bd-turism.firebaseapp.com",
-    projectId: "bd-turism",
-    storageBucket: "bd-turism.appspot.com",
-    messagingSenderId: "113554870401",
-    appId: "1:113554870401:web:bf6ac649b26b4182c07d80"
+
 };
 
 export default firebaseConfig
